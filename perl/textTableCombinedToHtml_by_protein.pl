b4d514cf3d3febd7cb503cc14c79c99c7b2f4601
Jinjun_Gao <gaojinjun@pku.edu.cn>
2015-09-26 11:13:05 +0800

    add histogram to the Html by protein

--- perl/textTableCombinedToHtml_by_protein.pl
+++ perl/textTableCombinedToHtml_by_protein.pl
@@ -50,6 +50,7 @@ print OUTFILE "<A HREF=\"combined_averaged_ratios.txt\">Averaged Ratios Table(to
 
 print OUTFILE "<A HREF=\"http://$hostname/cgi-bin/chuquest/batch_annotate.pl?dset=$cwd/$intable\">Gabe's Uniprot Batch Annotation</A><BR><BR>\n";
 print OUTFILE "<A HREF=\"$ARGV[0]\.png\">Ratio Plot</A><BR><BR>\n";
+print OUTFILE "<A HREF=\"$ARGV[0]\_histogram\.png\">Histogram</A><BR><BR>\n";
 if( -e "$ARGV[0].vennDiagram.png" ) {
     print OUTFILE "<A HREF=\"$ARGV[0]\.vennDiagram\.png\">Venn Diagram</A><BR><BR>\n";
 }
